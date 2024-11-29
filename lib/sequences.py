42211e019d80e999cf8b5ececced2c3a3dfdb789
Eric Richard <reric0802@gmail.com>
2024-11-29 05:06:37 +0300

    added code to create fibonnaci numbers

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    if length == 0:
+        return print('[]')
+    elif length == 1:
+        return print('[0]')
+    elif length == 2:
+        return print(fibonacci_sequence)
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    return print(fibonacci_sequence)
+
+print_fibonacci(5)
+
